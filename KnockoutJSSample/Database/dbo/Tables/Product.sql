6c4d90a8f7c69cf6ac6dbb3256acd0ec6dc26815
rmjahangeer <callrmj@gmail.com>
2018-06-21 01:28:31 +0500

    db project added

---
+++ KnockoutJSSample/Database/dbo/Tables/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX) NOT NULL,
+    [CategoryId] INT            NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Product_Category] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+);
+
